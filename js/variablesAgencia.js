d6e9764b3c7227c07a5abcdd0f9702ac14beb13b
Mall Virtual <39660941+AgenciaMV@users.noreply.github.com>
2019-03-03 19:44:44 -0600

    2
    2

---
+++ js/variablesAgencia.js
@@ -0,0 +1,28 @@
+//var nombreCliente = "Fumigadora Victoria";
+
+
+var nombreCliente = "Electric ResidCom";
+var telefonoFijo = "8822-5738";
+var telefonoCelular = "8822-5738";
+var eMail = "emg-92@outlook.com";
+
+
+
+//NOMBRE DEL CLIENTE
+document.getElementById("nC").innerHTML = nombreCliente;
+//NUMERO TELEFONO FIJO
+document.getElementById("telFijo").innerHTML = telefonoFijo;
+//NUMERO TELEFONO WHATSAPP
+document.getElementById("telCelular").innerHTML = telefonoCelular;
+//CORREO ELECTRONICO
+document.getElementById("correoMv").innerHTML = eMail;
+//TITULO DE PORTADA 
+
+
+
+
+
+
+
+
+
